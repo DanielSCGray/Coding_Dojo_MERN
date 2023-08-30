a946429f70467fe4109d7303b620ffd9b3b4f928
DanielSCGray <138812633+DanielSCGray@users.noreply.github.com>
2023-08-30 15:28:31 -0700

    hook form assignment done

---
+++ React/practice3_hook_form/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import HookForm from './components/HookForm';
+//HookForm.js was renamed, making the error above inaccurate.
+
+function App() {
+  return (
+    <div className="App">
+      <HookForm />
+    </div>
+  );
+}
+
+export default App;
