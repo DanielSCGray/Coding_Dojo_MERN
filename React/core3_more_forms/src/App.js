bbeef198a744b2a0ab4c1cc7f4574904a2c6c67a
DanielSCGray <138812633+DanielSCGray@users.noreply.github.com>
2023-08-31 11:41:59 -0700

    form w/validation using state

---
+++ React/core3_more_forms/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Form from './components/Form';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import BasicExample from './components/Basic';
+
+function App() {
+    return (
+
+        <div className="App">
+            <Form />
+            
+        </div>
+    );
+}
+
+export default App;
---
+++ React/core3_more_forms/src/components/Form.js
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Form = props => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const firstNameVal = () => {
+        if (firstName.length === 1) {
+            return ('First name must be at least 2 characters')
+        }
+        else { return ('') }
+    }
+    const lastNameVal = () => {
+        if (lastName.length === 1) {
+            return ('Last name must be at least 2 characters')
+        }
+        else { return ('') }
+    }
+    const emailVal = () => {
+        if (email.length === 1) {
+            return ('Email must be at least 2 characters')
+        }
+        else { return ('') }
+    }
+    const passwordVal = () => {
+        if (password.length >0 && password.length <8) {
+            return ('Pasword must be at least 8 characters')
+        }
+        else { return ('') }
+    }
+    const confirmPasswordVal = () => {
+        if (confirmPassword.length> 0 && password !== confirmPassword) {
+            return ('Paswords must match')
+        }
+        else { return ('') }
+    }
+
+    return (
+        <div>
+            <form>
+                <div>
+                    <label>First Name: </label>
+                    <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                    <p>{firstNameVal()}</p>
+                </div>
+                <div>
+                    <label>Last Name: </label>
+                    <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                    <p>{lastNameVal()}</p>
+                </div>
+                <div>
+                    <label>Email Address: </label>
+                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <p>{emailVal()}</p>
+                </div>
+                <div>
+                    <label>Password: </label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <p>{passwordVal()}</p>
+                </div>
+                <div>
+                    <label>Confirm Password: </label>
+                    <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                    <p>{confirmPasswordVal()}</p>
+                </div>
+            </form>
+
+        </div>
+    )
+}
+
+export default Form;
