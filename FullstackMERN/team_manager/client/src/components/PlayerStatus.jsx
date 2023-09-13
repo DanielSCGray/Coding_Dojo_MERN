9a9af186b7f6c2685bb1839c282df930088e80d2
DanielSCGray <138812633+DanielSCGray@users.noreply.github.com>
2023-09-13 16:48:46 -0700

    fixed live updates

--- FullstackMERN/team_manager/client/src/components/PlayerStatus.jsx
+++ FullstackMERN/team_manager/client/src/components/PlayerStatus.jsx
@@ -15,20 +15,22 @@ export const PlayerStatus = (props) => {
         .catch(err => console.log(err))
 }, []);
 
-    const updatePlayer = (targetID, gameID, newStatus) => {
+    const updatePlayer = (idx, targetID, gameID, newStatus) => {
         axios.patch(`http://localhost:8000/api/players/${targetID}`, {
             [gameID] : newStatus
         })
-        .then( updateDOM(targetID, gameID, newStatus))
+        .then( updateDOM(idx, targetID, gameID, newStatus))
         .catch(err => console.log(err))
     }
 
-    const updateDOM = (id, game, stat) => {
+    const updateDOM = (changeidx, id, game, stat) => {
+        const preList = allPlayers.slice(0, changeidx);
+        const postList = allPlayers.slice((changeidx+1));
         const updatedPlayer = allPlayers.filter(eachPlayer => eachPlayer._id == id)
         console.log(allPlayers)
         updatedPlayer[0][game] = stat;
-        const filteredPlayers = allPlayers.filter(eachPlayer => eachPlayer._id !== id)
-        setAllPlayers([...filteredPlayers, updatedPlayer[0]])
+        
+        setAllPlayers([...preList, updatedPlayer[0], ...postList])
     }
     const removeFromDOM = deletedID => {
         const filteredList = allPlayers.filter((eachPlayer, idx) => eachPlayer._id !== deletedID)
@@ -39,36 +41,36 @@ export const PlayerStatus = (props) => {
     // console.log(gameID)
 
     
-    const buttonCheck1 = player => {
+    const buttonCheck1 = (player, idx) => {
         if (player[gameID] == 'playing') {
             return(
-                <button className='btn btn-success btn-sm m-2' onClick={ () => updatePlayer(player._id, gameID, 'playing')}>Playing</button>
+                <button className='btn btn-success btn-sm m-2' onClick={ () => updatePlayer(idx, player._id, gameID, 'playing')}>Playing</button>
             )
         } else{
             return (
-                <button className='btn btn-light btn-sm m-2' onClick={ () => updatePlayer(player._id, gameID, 'playing')}>Playing</button>
+                <button className='btn btn-light btn-sm m-2' onClick={ () => updatePlayer(idx, player._id, gameID, 'playing')}>Playing</button>
             )
         }
     }
-    const buttonCheck2 = player => {
+    const buttonCheck2 = (player, idx) => {
         if (player[gameID] == 'not playing') {
             return(
-                <button className='btn btn-danger btn-sm m-2' onClick={ () => updatePlayer(player._id, gameID, 'not playing')}>Not Playing</button>
+                <button className='btn btn-danger btn-sm m-2' onClick={ () => updatePlayer(idx, player._id, gameID, 'not playing')}>Not Playing</button>
             )
         } else{
             return (
-                <button className='btn btn-light btn-sm m-2' onClick={ () => updatePlayer(player._id, gameID, 'not playing')}>Not Playing</button>
+                <button className='btn btn-light btn-sm m-2' onClick={ () => updatePlayer(idx, player._id, gameID, 'not playing')}>Not Playing</button>
             )
         }
     }
-    const buttonCheck3 = player => {
+    const buttonCheck3 = (player, idx) => {
         if (player[gameID] == 'undecided') {
             return(
-                <button className='btn btn-warning btn-sm m-2' onClick={ () => updatePlayer(player._id, gameID, 'undecided')}>Undecided</button>
+                <button className='btn btn-warning btn-sm m-2' onClick={ () => updatePlayer(idx, player._id, gameID, 'undecided')}>Undecided</button>
             )
         } else{
             return (
-                <button className='btn btn-light btn-sm m-2' onClick={ () => updatePlayer(player._id, gameID, 'undecided')}>Undecided</button>
+                <button className='btn btn-light btn-sm m-2' onClick={ () => updatePlayer(idx, player._id, gameID, 'undecided')}>Undecided</button>
             )
         }
     }
@@ -94,9 +96,9 @@ export const PlayerStatus = (props) => {
                                 {/* <button className='btn btn-danger btn-sm m-2' onClick={ () => updatePlayer(eachPlayer._id, gameID, 'playing')}>Playing</button>
                                 <button className='btn btn-danger btn-sm m-2' onClick={ () => updatePlayer(eachPlayer._id, gameID, 'not playing')}>Not Playing</button>
                                 <button className='btn btn-danger btn-sm m-2' onClick={ () => updatePlayer(eachPlayer._id, gameID, 'undecided')}>Undecided</button> */}
-                                {buttonCheck1(eachPlayer)}
-                                {buttonCheck2(eachPlayer)}
-                                {buttonCheck3(eachPlayer)}
+                                {buttonCheck1(eachPlayer, idx)}
+                                {buttonCheck2(eachPlayer, idx)}
+                                {buttonCheck3(eachPlayer, idx)}
                             </td>
                         </tr>
                     )
